e65e7bf486f0132ef6728d5665e8d8f87c351324
servosdesign <servos.design0@gmail.com>
2023-02-25 09:30:25 -0600

    theme hex in js

---
+++ themes/ruCream-color.js
@@ -0,0 +1,18 @@
+const colors = {
+  vue: {
+    functions: '#FF7C7C',
+    variables: '#5C6166',
+    this: '#A7A7A7',
+    operators: '#FF7C7C',
+    numbers: '#CEC5A6',
+    strings: '#A7A7A7',
+    languageConstants: '#CEC5A6',
+    templatePairs: '#55B4D4',
+    attributeNames: '#F2AE49',
+    keywordControl: '#FF7C7C',
+    css: 'Ayu light'
+  },
+  others: {
+    default: 'Ayu light'
+  }
+}
